200bdf492dc32708ac9ad53b40e80093964ae427
SiFalorSum <nilshansander@hotmail.com>
2017-04-23 22:11:31 +0200

    gameEngine and translation
    gameEngine contains functions related to the game mechanics. translation
    is from a previous project and can be recycled with some modifications.

---
+++ gameEngine.js
@@ -0,0 +1,238 @@
+/*GAMEENGINE
+Functions related to the game mechanics.
+List of functions (UPDATE!):
+-generateTable(game)
+-generateTiles(game)
+-scramble(repeats)
+-isSolved(game)
+-Game(sizeX, sizeY, entryRow, exitRow, tileTypeArray)
+-getGame(difficulty, levelIndex)
+TODO: move tiles (custom drag-drop?), game score,
+game "master" function? et c...
+*/
+
+/* GENERATETABLE(GAME)
+Appends table row (<tr>) and table cell (<td>) elements to
+<table> element.
+
+Author: Nils Hansander
+Last modified: April 23, 2017
+*/
+function generateTable(game) {
+	var tab = document.getElementsByTagName(“table”);
+	for(y=0; y<game.sizeY; y++) {
+		var row = document.createElement(“tr”);
+		for(x=0; x<game.sizeX; x++) {
+			var el = document.createElement(“td”);
+			el.className = “col” + x + “ row” + y;
+			row.appendChild(el);
+		}
+		tab.appendChild(row);
+	}
+}
+
+
+/* GENERATETILES(GAME)
+Appends "tiles", ie <div> elements to table cells.
+
+Author: Nils Hansander
+Last modified: April 23, 2017
+*/
+function generateTiles(game) {
+	for(y=0; y<game.sizeY; y++) {
+		var row = document.getElementsByClassName(“row” + y);
+		for(x=0; x<game.sizeX; x++) {
+			var el = row.getElementsByClassName(“col” + x);
+            if(game.tileTypeArray[y*game.sixeX + x] !== "CC") {
+                var tile = document.createElement(“div”);
+                tile.className = “tile “ + 
+                    game.tileTypeArray[y*game.sixeX + x];
+                el.appendChild(tile);
+            } else {
+                el.className += "current";
+            }
+		}
+	}
+}
+
+
+/* SCRAMBLE(REPEATS)
+Makes "repeats" # of alternating vertical and horizontal moves
+of random length.
+
+**Check isSolved for scrambled game! 
+
+Author: Nils Hansander
+Last modified: April 18, 2017
+*/
+function scramble(repeats) {
+    var pos;        //(int) nodelist indices: current and drop target
+    var targ;       //Nodelist of table elements
+    var el2Move;    //Nodelist of "tile" div elements to move
+    var moveDir;    //Affect index of target node to append "tile"
+    //isRow determines if table row or column elements are processed
+    var isRow = Math.random() < 0.5;
+    var curr = document.getElementsByClassName(“current”);
+    curr.classList.remove("current");
+    
+    for(i=0; i<repeats; i++) {
+        //currently empty row or column index
+        pos[0] = parseInt(curr.classList.item(isRow ? 0:1).charAt(3));
+        //retrieves row or column nodelist
+        targ = document.getElementsByClassName(curr.classList.item(isRow ? 1:0));
+        //selects random index, except pos[0]
+        pos[1] = (pos[0] + Math.floor(Math.random()*(targ.length-2)) + 1)%targ.length;
+        curr = targ[pos[1]];
+        
+        moveDir = pos[1] < pos[0] ? 0:1;
+        pos[moveDir]++;
+        targ = targ.slice.apply(this, pos.sort()); //!! "this" may refer to global object?
+        el2Move = targ.children;
+        
+        for(j=1; j<targ.length; j++) {
+            targ[j-moveDir].appendChild(el2Move[j-1]);
+        }
+        isRow = !isRow;
+    }
+    curr.classList.add("current");
+}
+
+
+/* ISSOLVED(GAME)
+Returns "true" if the puzzle is solved. Also stores path of
+connected rail pieces in game.connectedTiles (Array).
+
+Author: Nils Hansander
+Last modified: April 23, 2017
+*/
+function isSolved(game) {
+	var checkDir = {
+		E = 'W', W = 'E', N = 'S', S = 'N'
+	}
+	//temporary div created to fill "current" empty cell.
+    var temp = document.createElement(“div”);
+	temp.className = “tile CC”;
+	document.getElementsByClassName(“current”).appendChild(temp);
+    //retrieves all "tile" divs in a nodelist
+	var tiles = document.getElementsByClassName(“tile”);
+
+	var x = 0;
+	var y = game.entryRow;
+	var lastDir = “E”;
+    game.connectedTiles = [];
+    
+	do {
+		if(x<0 || x>game.sizeX-1 || y<0 || y>game.sizeY-1) {
+			return false;
+		} else {
+			trackDir = game.tileTypeArray[y*game.sizeX + x].classList.item(1);
+            
+			if(trackDir.charAt(0) == checkDir[lastDir]) {
+				lastDir = trackDir.charAt(1);
+                //Push to array of rail connections
+                game.connectedTiles.push(trackDir);
+			} else if(trackDir.charAt(1) == checkDir[lastDir]){
+				lastDir = trackDir.charAt(0);
+                game.connectedTiles.push(trackDir.charAt(1) + lastDir);
+			} else {
+				return false;
+			}
+		}
+		if(lastDir == “E”) {
+			x++;
+		} else if(lastDir == “W”) {
+			x--;
+		} else if(lastDir == “N”) {
+			y--;
+		} else {
+			y++;
+		}
+	} while( !(x == game.sizeX && y == game.exitRow) );
+	document.getElementsByClassName(“current”).removeChild(temp);
+	return true;
+}
+
+/* GAME(SIZEX, SIZEY, ENTRYROW, EXITROW, TILETYPEARRAY)
+Constructor for the Game object. Object parameters are
+"sizeX" (width), "sizeY" (height), "entryRow" (row index of
+leftmost column, where path begins), "exitRow" (row index of
+rightmost column, where path ends), "tileTypeArray"
+(an array consisting of the following 2-char strings:
+WE, NS, SE, SW, NE, NW, XX (plain tile), CC (empty cell).
+These strings will define the railroad track layout),
+and "connectedTiles" (array of connected tile types,
+defined in isSolved()).
+
+Author: Nils Hansander
+Last modified: April 21, 2017
+*/
+function Game(sizeX, sizeY, entryRow, exitRow, tileTypeArray) {
+	this.sizeX = sizeX;
+	this.sizeY = sizeY;
+	this.entryRow = entryRow;
+	this.exitRow = exitRow;
+	this.tileTypeArray = tileTypeArray;
+	this.connectedTiles;
+    //this.timer;
+}
+
+
+/*GETGAME(DIFFICULTY, LEVELINDEX)
+Takes input "difficulty" (String) and "levelIndex" (int)
+and returns a Game object. "difficulty" can have the values
+"easy", "medium", or "hard".
+
+Author: Nils Hansander
+Last modified: April 23, 2017
+*/
+function getGame(difficulty, levelIndex) {
+    var easy1 = new Game(4, 4, 2, 1, [
+                "XX", "XX", "CC", "XX", 
+				"SE", "SW", "SE", "WE",
+				"NW", "NS", "NE", "SW",
+				"XX", "NE", "WE", "NW"	]);
+    var easy2 = new Game(6, 4, 2, 1, [
+                "", "", "", "", "", "",
+				"", "", "", "", "", "",
+                "", "", "", "", "", "",
+				"", "", "", "", "", ""	]);
+    var medium1 = new Game(6, 6, 3, 2, [
+                "", "", "", "", "", "",
+				"", "", "", "", "", "",
+                "", "", "", "", "", "",
+				"", "", "", "", "", "",
+                "", "", "", "", "", "",
+				"", "", "", "", "", ""	]);
+    var medium2 = new Game(8, 6, 3, 2, [
+                "", "", "", "", "", "", "", "",
+				"", "", "", "", "", "", "", "",
+                "", "", "", "", "", "", "", "",
+				"", "", "", "", "", "", "", "",
+                "", "", "", "", "", "", "", "",
+				"", "", "", "", "", "", "", ""	]);
+    var hard1 = new Game(8, 8, 4, 3, [
+                "", "", "", "", "", "", "", "",
+				"", "", "", "", "", "", "", "",
+                "", "", "", "", "", "", "", "",
+				"", "", "", "", "", "", "", "",
+                "", "", "", "", "", "", "", "",
+				"", "", "", "", "", "", "", "",
+                "", "", "", "", "", "", "", "",
+				"", "", "", "", "", "", "", ""	]);
+    var hard2 = new Game(10, 8, 4, 3, [
+                "", "", "", "", "", "", "", "", "", "",
+				"", "", "", "", "", "", "", "", "", "",
+                "", "", "", "", "", "", "", "", "", "",
+				"", "", "", "", "", "", "", "", "", "",
+                "", "", "", "", "", "", "", "", "", "",
+				"", "", "", "", "", "", "", "", "", "",
+                "", "", "", "", "", "", "", "", "", "",
+				"", "", "", "", "", "", "", "", "", ""	]);
+    
+    var games = {
+        easy: [easy1, easy2],
+        medium: [medium1, medium2],
+        hard: [hard1, hard2]
+    }
+    return games[difficulty][levelIndex];
+}
---
+++ translation.js
@@ -0,0 +1,297 @@
+//---translation---
+
+function dictionary() {
+    var dict = {
+/*        langTemplate: {
+            // translatable elements for admin_addusers.html
+            
+            // translatable elements for adminallbeverages.html
+            
+            // translatable elements for adminbeverages.html
+            
+            // translatable elements for adminhistory.html
+            
+            // translatable elements for adminusers.html
+            
+            // translatable elements for allbeverages.html
+            
+            // translatable elements for history.html
+            
+            // translatable elements for index.html
+            loginHead: '',
+            userLabel: '',
+            uname: '',
+            pswLabel: '',
+            psw: '',
+            personalButton: '',
+            staffButton: ''
+        },
+*/
+        sv: {
+            /* Index page */
+            username_label_index: ' Användarnamn',
+            username_placeholder_index: 'Användarnamn',
+            password_label_index: ' Lösenord',
+            password_placeholder_index: 'Lösenord',
+            login_button: 'Logga in',
+            
+            /* ---- Header for all pages ----*/
+            /* nav links */
+            all_beverages: 'Alla drycker',
+            previously_bought: 'Tidigare köp',
+            manage_users: 'Hantera användare',
+            manage_beverages: 'Hantera drycker',
+            edit_users: 'Redigera användare',
+            edit_balance: 'Redigera användarsaldo',
+            edit_slots: 'Redigera tillgängliga drycker',
+            edit_stock_price: 'Redigera lager/pris',
+            /* User info in header */
+            log_out: 'Logga ut',
+            welcome_p: 'Välkommen  ',
+            user_balance_p: 'Saldo:  ',
+            
+            /* All beverages page */
+            purchase_p: 'För att köpa en dryck, klicka på Buy-knappen eller dra och släpp en dryck i kundvagnen',
+            beer_cat: 'Öl',
+            cider_cat: 'Cider',
+            wine_cat: 'Vin',
+            non_cat: 'Alkoholfritt',
+            heading_cart: 'Kundvagn',
+            totalamount: 'Totalt:',
+            purchase_button: 'Köp',
+            clear_button: 'Töm kundvagn',
+            
+            /* Previously bought/history page */
+            prev_bought_drinks: 'Tidigare köpta drycker',
+            drink_th: 'Dryck',
+            timestamp_th: 'Tidsstämpel',
+            price_th: 'Pris',
+            
+            /* Manage user - 'Add user' & 'Edit user' tableheaders */
+            username_th: 'Användarnamn',
+            first_name_th: 'Förnamn',
+            last_name_th: 'Efternamn',
+            email_th: 'Email',
+            phone_th: 'Telefonnr',
+			add_user_instruction: 'Fyll i följande fält för att lägga till en ny användare. Alla fält måste fyllas i.',
+			edit_user_instruction: 'Klicka på edit-knappen bredvid din utvalda användare och ändra fälten som du vill uppdatera för att redigera en användare. Alla fält måste fyllas i och användarnamnet går inte att ändra.',
+			edit_balance_instruction: 'Klicka på edit-knappen bredvid din utvalda användare och ange ett nytt saldo för att redigera användarsaldot. Båda fälten måste fyllas i.',
+			edit_stock_instruction: 'Klicka på edit-knappen bredvid din utvalda dryck och ändra fälten som du vill uppdatera för att redigera en dryck. Alla fält måste fyllas i.',
+            /* 'Edit balance' page tablehead */
+            quantity_th: 'Antal i lager',
+            /* 'Edit user and balance' page tablehead */
+            balance_th: 'Saldo',
+            user_added_msg: 'Användare tillagd!',
+
+            all_users_and_balance: 'Alla användare och deras saldo',
+            all_users: 'Alla användare',
+            edit_user_balance: 'Redigera användarsaldo',
+            edit_user: 'Redigera användare',
+            add_user: 'Lägg till användare',
+            username_p: 'Användarnamn',
+            usernameBalance: 'Användarnamn',
+            usernameBalance_p: 'Saldo',
+            /*  */
+            admin_history: 'Tidigare köp',
+            add_users: 'Lägg till användare',
+            user_edited_msg: 'Användare uppdaterad!',
+            balance_edited_msg: 'Saldo uppdaterat!',
+            bev_updated_msg: 'Dryck uppdaterad!',
+
+            //Balance: 'Saldo:  ',
+            beverage_choose: 'Välj vilka drycker som finns i automaten',
+            save_button: 'Spara ändringar',
+            to_purchase: 'För att välja en dryck att köpa: dra och släpp en dryck i kundvagnen',
+            users_category: 'Användarnamn Förnamn Efternamn Email Telefon',
+            first_name: 'Förnamn',
+            fname: 'Förnamn', //placeholder
+            last_name: 'Efternamn',
+            lname: 'Efternamn', //placeholder
+            username: 'Användarnamn',
+            password: 'Lösenord',
+            email: 'Email',
+            emailplaceholder:'Email',
+            phone: 'Telefonnr',
+            phoneplaceholder:'Telefonnr',
+            saveuser_button: 'Spara användare',
+            beverageh3: 'Drycker',
+            bev_category: 'Dryck Pris Antal',
+            edit_beverage: 'Redigera dryck',
+            beveragetxt: 'Dryck',
+            beveragePlaceholder: 'Välj ny dryck',
+            quantitytxt: 'Antal',
+            quantityPlaceholder: 'Välj nytt antal',
+            pricetxt: 'Pris',
+            pricePlaceholder: 'Välj nytt pris',
+            savebeverage_button: 'Spara dryck',
+            savebalance_button: 'Spara nytt saldo',
+			
+            
+            // translatable elements for allbeverages.html
+        },
+        en: {
+            /* Index page */
+            username_label_index: ' Username',
+            username_placeholder_index: 'Username',
+            password_label_index: ' Password',
+            password_placeholder_index: 'Password',
+            login_button: 'Log in',
+            
+            /* ---- Header for all pages ----*/
+            /* nav links */
+            all_beverages: 'All Beverages',
+            previously_bought: 'Previously Bought',
+            manage_users: 'Manage Users',
+            manage_beverages: 'Manage Beverages',
+            add_users: 'Add User',
+            edit_users: 'Edit User',
+            edit_balance: 'Edit Balance',
+            edit_slots: 'Edit Slots',
+            edit_stock_price: 'Edit Stock/Price',
+            /* User info in header */
+            log_out: 'Log out',
+            welcome_p: 'Welcome  ',
+            user_balance_p: 'Balance:  ',
+            
+            /* All beverages page */
+            purchase_p: 'To choose beverages to purchase: click the Buy-button or drag and drop a drink into the shopping cart',
+            beer_cat: 'Beer',
+            cider_cat: 'Cider',
+            wine_cat: 'Wine',
+            non_cat: 'Non-Alcoholic',
+            heading_cart: 'Shopping cart',
+            totalamount: 'Total:',
+            purchase_button: 'Purchase',
+            clear_button: 'Clear',
+            
+            /* Previously bought/history page */
+            prev_bought_drinks: 'Previosly bought drinks',
+            drink_th: 'Drink',
+            timestamp_th: 'Time bought',
+            price_th: 'Price',
+            
+            /* Manage user - 'Add user' & 'Edit user' tableheaders */
+            username_th: 'Username',
+            first_name_th: 'First Name',
+            last_name_th: 'Last Name',
+            email_th: 'Email',
+            phone_th: 'Phone number',
+			add_user_instruction: 'To add a new user, enter the following details. All fields must be filled out.',
+			edit_user_instruction: 'To edit a user, click the edit button next to your desired user and change the fields that you want to update. All fields must be filled out and username can not be changed.',
+			edit_balance_instruction: 'To edit the balance of a user, click the edit button next to your desired user and enter a new balance.<br /> Both fields must be filled out.',
+			edit_stock_instruction: 'To edit a beverage, click the edit button next to your desired beverage and enter your new information. All fields must be filled out.',
+            /* 'Edit balance' page tablehead */
+            quantity_th: 'Quantity in stock',
+            /* 'Edit user and balance' page tablehead */
+            balance_th: 'Balance',
+            user_added_msg: 'User added!',
+            user_edited_msg: 'User updated!',
+            balance_edited_msg: 'Balance updated!',
+            bev_updated_msg: 'Beverage updated!',
+            
+            /*  */
+            all_users_and_balance: 'All users and their balance',
+            all_users: 'All users',
+            edit_user_balance: ' Edit user balance',
+            edit_user: ' Edit user',
+            add_user: 'Add user',
+            username_p: 'Username',
+            usernameBalance: 'Username',
+            usernameBalance_p: 'Balance',
+            admin_history: 'Previously Bought',
+            
+            //Balance: 'Balance:  ',
+            beverage_choose: 'Choose which beverages you want in the vending machine',
+            save_button: ' Save changes ',
+            //welcome: '<p id="welcome" class="transl">Welcome <span id="headerName"></span></p>',
+            to_purchase: 'To purchase a beverage, drag and drop a drink into the shopping cart',
+            users_category: 'Username Firstname Lastname Email Phone',
+            first_name: 'First name',
+            fname: 'First name', //placeholder
+            last_name: 'Last name',
+            lname: 'Last name', //placeholder
+            username: 'Username',
+            password: 'Password',
+            email: 'Email',
+            emailplaceholder:'Email',
+            phone: 'Phone number',
+            phoneplaceholder:'Phone number',
+            saveuser_button: 'Save user',
+            beverageh3: ' Beverages',
+            bev_category: 'Beverage Price Quantity',
+            edit_beverage: ' Edit Beverage',
+            beveragetxt: ' Beverage ',
+            beveragePlaceholder: 'Enter the beverage',
+            quantitytxt: ' Quantity ',
+            quantityPlaceholder: 'Enter a new quantity',
+            pricetxt: 'Price',
+            pricePlaceholder: 'Enter a new price',
+            savebeverage_button: 'Save beverage',
+            savebalance_button: 'Update user balance',
+            
+            // translatable elements for allbeverages.html
+        }    
+    }
+    return dict[sessionStorage.getItem('sprak')];
+}
+
+
+function translate() {
+    var textElem = document.getElementsByClassName('transl');
+    var dict = dictionary();
+    
+    for(var j=0; j<textElem.length; j++) {
+        var x = dict[textElem[j].id];
+        if(x === undefined) {
+            if(textElem[j].nodeName === 'INPUT') {
+                if(textElem[j].getAttribute('placeholder')) {
+                    x = textElem[j].getAttribute('placeholder');
+                }
+                else if(textElem[j].getAttribute('value')) {
+                    x = textElem[j].getAttribute('value');
+                }
+            }
+            else {
+                x = document.getElementById(textElem[j].id).innerHTML;
+            }
+            //console.log(textElem[j].id + ": '" + x + "'" + ","); //if find translations id:s uncomment this
+        }
+        else {
+            if(textElem[j].nodeName === 'INPUT') {
+                if(textElem[j].getAttribute('placeholder')) {
+                    textElem[j].setAttribute('placeholder', x);
+                }
+                else if(textElem[j].getAttribute('value')) {
+                    textElem[j].setAttribute('value', x);
+                }
+            }
+            else {
+                document.getElementById(textElem[j].id).innerHTML = x;
+            }
+        }
+    }
+}
+
+
+function langSelect() {
+    var y = document.getElementById("language_choice");
+    if (y == null) {
+        y = document.getElementById("language_choice_index");
+    }
+    var x = y.value;
+    sessionStorage.setItem('sprak', x);
+    translate();
+}
+
+
+function checkLang() {
+    if (!sessionStorage.getItem('sprak')) {
+        sessionStorage.setItem('sprak', 'en') // Default language
+    }
+    var x = document.getElementById("language_choice");
+    x.addEventListener("change", langSelect);
+    document.getElementById(sessionStorage.getItem('sprak')).selected = 'true';
+    translate();     
+}
+
+//-----------------
