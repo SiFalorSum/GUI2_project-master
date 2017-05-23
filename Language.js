7da9e37d90080520efcb8d62c060a8fd33125c57
Lejuan66 <hulejuan88@gmail.com>
2017-05-23 23:01:13 +0200

    Update language.js
    update language js file

--- Language.js
+++ Language.js
@@ -2,20 +2,84 @@
  * Created by lei on 19/05/17.*/
 
 function Chinese(){
-    document.getElementById("login").innerHTML="登陆";
+    //document.getElementById("login").innerHTML="登陆";
     document.getElementById("help").innerHTML="帮助";
     document.getElementById("levselection").innerHTML="等级";
+    document.getElementById("th").innerHTML="尝试";
+    document.getElementById("ts").innerHTML="尝试";
 }
 
 function Swedsih(){
-    document.getElementById("login").innerHTML="logga in";
+   // document.getElementById("login").innerHTML="logga in";
     document.getElementById("help").innerHTML="handledning";
     document.getElementById("levselection").innerHTML="nivå";
+    document.getElementById("th").innerHTML="försök";
+    document.getElementById("ts").innerHTML="försök";
 }
 
 
 function English(){
-    document.getElementById("login").innerHTML="log in";
+    //document.getElementById("login").innerHTML="log in";
     document.getElementById("help").innerHTML="Tutorial";
-    document.getElementById("levselection").innerHTML="level selection";
+    document.getElementById("levselection").innerHTML="level";
+    document.getElementById("th").innerHTML="try it";
+    document.getElementById("ts").innerHTML="try it";
+}
+
+function lChinese(){
+    document.getElementById("title").innerHTML = "选择等级";
+    document.getElementById("stopaudio").innerHTML = "停止音乐";
+    document.getElementById("startaudio").innerHTML = "开始音乐";
+    document.getElementById("volumeAdd").innerHTML = "调大音量";
+    document.getElementById("volumeDecrease").innerHTML = "降低音量";
+    document.getElementById("volumeAdd").innerHTML = "调大音量";
+    document.getElementById("backhome").innerHTML = "返回主页";
+    document.getElementById("l1").innerHTML = "等级1";
+    document.getElementById("l2").innerHTML = "等级2";
+    document.getElementById("l3").innerHTML = "等级3";
+    document.getElementById("l4").innerHTML = "等级4";
+    document.getElementById("l5").innerHTML = "等级5";
+    document.getElementById("l6").innerHTML = "等级6";
+    document.getElementById("l7").innerHTML = "等级7";
+    document.getElementById("l8").innerHTML = "等级8";
+    document.getElementById("l9").innerHTML = "等级9";
+    document.getElementById("l10").innerHTML = "等级10";
+}
+
+function lSwedish(){
+    document.getElementById("title").innerHTML = "Välj Nivå";
+    document.getElementById("stopaudio").innerHTML = "Sluta Musik";
+    document.getElementById("startaudio").innerHTML = "Starta Musik";
+    document.getElementById("volumeAdd").innerHTML = "öka Volymen";
+    document.getElementById("volumeDecrease").innerHTML = "Välj Nivå";
+    document.getElementById("backhome").innerHTML = "hemsida";
+    document.getElementById("l1").innerHTML = "Nivå 1";
+    document.getElementById("l2").innerHTML = "Nivå 2";
+    document.getElementById("l3").innerHTML = "Nivå 3";
+    document.getElementById("l4").innerHTML = "Nivå 4";
+    document.getElementById("l5").innerHTML = "Nivå 5";
+    document.getElementById("l6").innerHTML = "Nivå 6";
+    document.getElementById("l7").innerHTML = "Nivå 7";
+    document.getElementById("l8").innerHTML = "Nivå 8";
+    document.getElementById("l9").innerHTML = "Nivå 9";
+    document.getElementById("l10").innerHTML = "Nivå 10";
+}
+
+function lEnglish(){
+    document.getElementById("title").innerHTML = "Select Level";
+    document.getElementById("startaudio").innerHTML = "Start Music";
+    document.getElementById("volumeDecrease").innerHTML = "Volume down";
+    document.getElementById("volumeAdd").innerHTML = "Volume Up";
+    document.getElementById("stopaudio").innerHTML = "Stop Music";
+    document.getElementById("backhome").innerHTML = "Homepage";
+    document.getElementById("l1").innerHTML = "level 1";
+    document.getElementById("l2").innerHTML = "level 2";
+    document.getElementById("l3").innerHTML = "level 3";
+    document.getElementById("l4").innerHTML = "level 4";
+    document.getElementById("l5").innerHTML = "level 5";
+    document.getElementById("l6").innerHTML = "level 6";
+    document.getElementById("l7").innerHTML = "level 7";
+    document.getElementById("l8").innerHTML = "level 8";
+    document.getElementById("l9").innerHTML = "level 9";
+    document.getElementById("l10").innerHTML = "level 10";
 }
